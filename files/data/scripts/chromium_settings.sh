91f38c6405adce51ee50408fbb3dbcc74c1c006f
shadow <108803410+68p@users.noreply.github.com>
2024-10-08 15:33:09 -0400

    sync data partition files
    changes match what final data partition is

--- files/data/scripts/chromium_settings.sh
+++ files/data/scripts/chromium_settings.sh
@@ -1,6 +1,6 @@
 # settings for /scripts/start_chromium.sh
 
-URL="https://192.168.1.144:8123/lovelace/"
+URL="https://usenocturne.vercel.app/"
 SCALE="1.0"
 EXTRA_CHROMIUM_ARGS=""
 EXTRA_XORG_ARGS="-nocursor"
