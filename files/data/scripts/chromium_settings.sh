333aa6013bb9d37a4731af13a3d3b40008137fe8
shadow <108803410+68p@users.noreply.github.com>
2024-11-14 15:43:28 -0500

    fix: update links for build image

--- files/data/scripts/chromium_settings.sh
+++ files/data/scripts/chromium_settings.sh
@@ -1,6 +1,6 @@
 # settings for /scripts/start_chromium.sh
 
-URL="https://usenocturne.vercel.app/"
+URL="https://nocturne.brandons.place/"
 SCALE="1.0"
 EXTRA_CHROMIUM_ARGS=""
 EXTRA_XORG_ARGS="-nocursor"
