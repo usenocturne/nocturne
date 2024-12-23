41a1b5c98c4014c003b2866d69e9fcfe93fd1c95
ELLEgant <paris.adney@gmail.com>
2024-12-23 14:15:35 -0600

    refactor: Added additional preflight checks, ability to select WAN_INTERFACE, specified support for only Raspbian Bullseye, and added a conditional check for portforwarding to the CarThing

--- setup-scripts/setup_host_rpi.sh
+++ setup-scripts/setup_host_rpi.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-# Sets up a raspberry pi to provide internet for USB device connected
-# This should be run ONCE on the host machine
+#SCRIPT NAME: setup_host_rpi
+#AUTHOR(s): ELLEgant
+#DATE: 12/23/2024
+#DESCRIPTION: Configures a Raspberry Pi running Debian Bullseye to be a network passthrough device for the Spotify CarThing running custom firmware.
 
 set -e  # bail on any errors
 
@@ -11,13 +13,14 @@ export DEBIAN_FRONTEND=noninteractive
 # VARIABLES
 HOST_NAME="superbird"
 USBNET_PREFIX="192.168.7"  # usb network will use .1 as host device, and .2 for superbird
-CT_INTERFACE="usb0"
-WAN_INTERFACE="wlan0"
+CT_INTERFACE="usb0" # Typically usb0 but can appear as eth0 depending on network manager. This assumption will be programatically determined at a later date
+WAN_INTERFACE="wlan0" # Assuming you have a PiZero it's always going to be wlan0 unless you connect a USB NIC.
+NET_ADAPTERS=($(ip link | grep -oE '\b(eth[0-9]+|wlan[0-9]+)\b'))
+DEV_MODE=0
 
 # FUNCTIONS
-
 function remove_if_exists() {
-    # remove a file if it exists
+    # Remove a file if it exists
     FILEPATH="$1"
     if [ -f "$FILEPATH" ]; then
         echo "found ${FILEPATH}, removing"
@@ -26,7 +29,7 @@ function remove_if_exists() {
 }
 
 function append_if_missing() {
-    # append string to file only if it does not already exist in the file
+    # Append string to file only if it does not already exist in the file
     STRING="$1"
     FILEPATH="$2"
     grep -q "$STRING" "$FILEPATH" || {
@@ -39,9 +42,9 @@ function append_if_missing() {
 }
 
 function forward_port() {
-    # usage: forward_port <host port> <superbird port>
-    # forward a tcp port to access service on superbird via host
-    #   if no superbird port is provided, same port number is used for both
+    # Usage: forward_port <host port> <superbird port>
+    # Forwards a tcp port to access service on superbird via host.
+    # If no superbird port is provided, same port number is used for both.
     SOURCE="$1"
     DEST="$2"
     if [ -z "$DEST" ]; then
@@ -65,8 +68,14 @@ if [ "$(uname -s)" != "Linux" ]; then
     exit 1
 fi
 
+# Does not run on anything but Raspbian Bullseye
+if ! lsb_release -a | grep -q "Raspbian GNU/Linux 11 (bullseye)"; then
+    echo "Current OS not supported!"
+    exit 1
+fi
+
 # Detect if usb0 CarThing NIC is already configured.
-if ! ip addr show "${CT_INTERFACE}" | grep -q "inet "; then
+if ! ip addr show "${CT_INTERFACE}" | grep -q "192.168.1 "; then
     echo "No inactive network interface found. This may occur if the script was already run, or if your Spotify Car Thing is not plugged in."
     exit 1
 fi
@@ -80,9 +89,59 @@ else
     exit 1
 fi
 
+# Detect if multiple NIC are present and select one
+if [ "${#NET_ADAPTERS[@]}" -gt 1 ]; then
+    NIC_ID=0
+    echo "Multiple NICs found, please choose a network adapter that is connected to the internet"
+    echo "Your options are: "
+    for i in ${!NET_ADAPTERS[@]}; do
+        echo "$i - ${NET_ADAPTERS[i]}"
+    done
+    echo ""
+    read -p "Choice: " NIC_ID
+    WAN_INTERFACE="${NET_ADAPTERS[NIC_ID]}"
+fi
+
+# If multiple NICs are not present set default WAN_INTERFACE
+if printf '%s\n' "${NET_ADAPTERS[@]}" | grep -q '^eth'; then
+    # Set NIC to only available eth interface
+    WAN_INTERFACE="${NET_ADAPTERS[0]}"
+    echo "Only one Ethernet interface detected, using $WAN_INTERFACE..."
+else
+    # Set NIC to only available wlan interface
+    WAN_INTERFACE="${NET_ADAPTERS[0]}"
+    echo "Only one WiFi interface detected, using $WAN_INTERFACE..."
+fi
+
+# Ask if user wants ports forwarded for ssh, vnc, and debugging
+read -p "Do you want to enable dev mode to have remote access to the CarThing? (y/N): " dev_response
+case "$dev_response" in
+    [Yy]* )
+        echo "Dev mode will be enabled!"
+        DEV_MODE=1
+        ;;
+    [Nn]* )
+        echo "Dev mode will not be enabled!"
+        DEV_MODE=0
+        ;;
+    * )
+        echo "Invalid input! Assuming no, dev mode will not be enabled!"
+        DEV_MODE=0
+        ;;
+esac
+
 # MAIN_SCRIPT
 
+# Update system
+echo "Updating repositories and packages..."
+apt -qq update -y && apt -qq upgrade -y
+
+# Gather deps
+echo "Installing deps..."
+apt install -qq iptables iptables-persistent -y
+
 # Fix usb enumeration when connecting superbird in maskroom mode
+echo "Applying USB fixes..."
 echo '# Amlogic S905 series can be booted up in Maskrom Mode, and it needs a rule to show up correctly' > /etc/udev/rules.d/70-carthing-maskrom-mode.rules
 echo 'SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="1b8e", ATTR{idProduct}=="c003", MODE:="0666", SYMLINK+="worldcup"' >> /etc/udev/rules.d/70-carthing-maskrom-mode.rules
 
@@ -91,6 +150,7 @@ remove_if_exists /lib/systemd/network/73-usb-net-by-mac.link
 remove_if_exists /lib/udev/rules.d/73-usb-net-by-mac.rules
 
 # Allow IP forwarding
+echo "Enabling IP forwarding..."
 append_if_missing "net.ipv4.ip_forward = 1" /etc/sysctl.conf || {
     sysctl -p  # reload from conf
 }
@@ -99,6 +159,8 @@ append_if_missing "net.ipv4.ip_forward = 1" /etc/sysctl.conf || {
 mkdir -p /etc/iptables
 
 # Clear all iptables rules
+echo "Clearing iptables..."
+
 iptables -F
 iptables -X
 iptables -Z 
@@ -110,15 +172,32 @@ iptables -t raw -F
 iptables -t raw -X
 
 # Rewrite iptables rules
+echo "Writing iptables rules..."
+
 iptables -P FORWARD ACCEPT
 iptables -A FORWARD -o "${WAN_INTERFACE}" -i "${CT_INTERFACE}" -s "${USBNET_PREFIX}.0/24" -m conntrack --ctstate NEW -j ACCEPT
 iptables -A FORWARD -o "${WAN_INTERFACE}" -i "${CT_INTERFACE}" -s "${USBNET_PREFIX}.0/24" -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A POSTROUTING -t nat -j MASQUERADE -s "${USBNET_PREFIX}.0/24"
 
-# persist rules to file
+# Forward ports if dev mode was enabled
+if [ "$DEV_MODE" -eq 1 ]; then
+    echo "Setting up Dev Mode..."
+    echo "Forwarding port: 2022:22"
+    forward_port 2022 22
+    echo "Forwarding port: 5900:5900"
+    forward_port 5900
+    echo "Forwarding port: 9222:9222"
+    forward_port 9222
+else
+    echo "Skipping Dev Mode setup..."
+fi
+
+# Save iptables rules to file
 iptables-save > /etc/iptables/rules.v4
 
-# write the usb network config
+# Write the config changes for usb network
+echo "Writing config for $CT_INTERFACE..."
+
 mkdir -p /etc/network/interfaces.d/
 
 cat << EOF > /etc/network/interfaces.d/usb0
@@ -129,7 +208,7 @@ iface usb0 inet static
 	netmask 255.255.255.0
 EOF
 
-# add superbird to /etc/hosts
+# Add superbird (CarThing) to /etc/hosts
 append_if_missing "${USBNET_PREFIX}.2  ${HOST_NAME}"  "/etc/hosts"
 
 echo "Need to reboot for all changes to take effect!"
