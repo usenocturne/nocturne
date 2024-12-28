usenocturne/nocturne

commit 83ad6b258c08861409e6ea22454c445d6667ef52

fix(setup_host): hopefully fix append_if_missing
may still have issues that i dont know, but it was tested a couple of times before pushing

setup-scripts/setup_host_rpi.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # Configures a Raspberry Pi to be a network passthrough device for the Spotify Car Thing running custom firmware.
-# TODO: find out why append_if_missing is not working as it should
 
 set -e # bail on any error
 
@@ -33,11 +32,9 @@ function append_if_missing() {
     FILEPATH="$2"
     grep -q "$STRING" "$FILEPATH" || {
         echo "appending \"$STRING\" to $FILEPATH"
-        echo "$STRING" >> "$FILEPATH"
-        return 1
+        printf "%s\n" "$STRING" >> "$FILEPATH"
     }
     echo "Already found \"$STRING\" in $FILEPATH"
-    return 0
 }
 
 # Usage: forward_port <host port> <superbird port>

commit b5a0de10c3e67bbc23909f2f609c53d67e8a1987

fix(setup_host): Create nocturne.conf instead of editing main sysctl.conf
There is a regression in 83ad6b258c08861409e6ea22454c445d6667ef52 which
causes `sysctl -p` to not be run. But we are already rebooting, for
interface setup, so this is not needed.

setup-scripts/setup_host_rpi.sh
@@ -130,9 +130,7 @@ remove_if_exists /lib/systemd/network/73-usb-net-by-mac.link
 remove_if_exists /lib/udev/rules.d/73-usb-net-by-mac.rules
 
 echo "Enabling IP forwarding..."
-append_if_missing "net.ipv4.ip_forward = 1" /etc/sysctl.conf || {
-    sysctl -p # reload from conf
-}
+echo "net.ipv4.ip_forward = 1" > /etc/sysctl.d/nocturne.conf
 
 # clear rules, and add new tables
 echo "Setting up nftables..."