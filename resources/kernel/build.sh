4268b98b6a1bdde3ada20a34d1b71bf613830367
Max Cooley <max@polygonal.place>
2025-04-16 16:10:12 -0700

    fix: resolve resources/kernel/build.sh script crashing

--- resources/kernel/build.sh
+++ resources/kernel/build.sh
@@ -11,5 +11,8 @@ fi
 sudo rm -rf ./output
 mkdir -p ./output
 
-docker run --privileged --rm -it -v nix-store:/nix -v nix-root:/root -v ./:/workdir "$(docker build -q .)" /usr/bin/env bash -c \
+IMAGE_ID=$(docker build .)
+
+docker run --privileged --rm -it -v nix-store:/nix -v nix-root:/root -v ./:/workdir "$IMAGE_ID" /usr/bin/env bash -c \
   "nix build github:JoeyEamigh/nixos-superbird#nixosConfigurations.headless-example.config.system.build.kernel --print-out-paths && cp -r result/* output/"
+
