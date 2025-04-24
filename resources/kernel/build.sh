6cde9198777733769e2beabf4a236f9d92f43b08
BBaoVanC <bbaovanc@bbaovanc.com>
2025-04-24 14:56:33 -0500

    kernel: Simplify build script by using docker compose

--- resources/kernel/build.sh
+++ resources/kernel/build.sh
@@ -1,17 +1,7 @@
 #!/bin/sh -x
 
-if [ ! "$(docker volume inspect nix-store 2> /dev/null)" ]; then
-  docker volume create nix-store
-fi
-
-if [ ! "$(docker volume inspect nix-root 2> /dev/null)" ]; then
-  docker volume create nix-root
-fi
-
 sudo rm -rf ./output
 mkdir -p ./output
 
-IMAGE_ID=$(docker build -q .)
-
-docker run --privileged --rm -it -v nix-store:/nix -v nix-root:/root -v ./:/workdir "$IMAGE_ID" /usr/bin/env bash -c \
-  "nix build github:JoeyEamigh/nixos-superbird#nixosConfigurations.headless-example.config.system.build.kernel --print-out-paths && cp -r result/* output/"
+docker compose build
+docker compose run --rm build
