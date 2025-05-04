638cba153c2184848135cb9ddc83d56b47c6de25
shadow <108803410+68p@users.noreply.github.com>
2025-05-04 14:04:10 -0400

    fix: bluetooth connect works properly now
    - battery info still doesnt work, but controlling the player on the phone is now possible

--- resources/scripts/stages/30/40-bluetooth.sh
+++ resources/scripts/stages/30/40-bluetooth.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-chroot_exec apk add bluez dbus
+chroot_exec apk add bluez dbus bluez-alsa alsa-utils
 
 mkdir -p "$ROOTFS_PATH"/lib/firmware/brcm
 cp "$RES_PATH"/firmware/brcm/* "$ROOTFS_PATH"/lib/firmware/brcm/
@@ -12,5 +12,5 @@ mkdir -p "$ROOTFS_PATH"/etc/bluetooth
 rm -f "$ROOTFS_PATH"/etc/bluetooth/main.conf
 cp "$RES_PATH"/config/bluetooth.conf "$ROOTFS_PATH"/etc/bluetooth/main.conf
 
-DEFAULT_SERVICES="${DEFAULT_SERVICES} dbus bluetooth bluetooth_adapter"
+DEFAULT_SERVICES="${DEFAULT_SERVICES} dbus bluetooth bluetooth_adapter bluealsa alsa"
 SYSINIT_SERVICES="${SYSINIT_SERVICES} superbird_init"
