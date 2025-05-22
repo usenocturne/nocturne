b29afc26812f82851bdbddd3942565d53be87d32
BBaoVanC <bbaovanc@bbaovanc.com>
2025-05-22 17:00:53 -0500

    fix(boot): Don't use hardcoded /dev/loop0 for mounting boot.bin

--- resources/scripts/stages/50/10-boot-bin.sh
+++ resources/scripts/stages/50/10-boot-bin.sh
@@ -8,8 +8,10 @@ mv "$WORK_PATH"/bootfs-blank.bin "$IMAGE_PATH"/boot.bin
 dd if="$RES_PATH"/flash/logo.bin of="$IMAGE_PATH"/boot.bin bs=1M seek=156 conv=notrunc
 
 mkdir -p /mnt/bootbin
-losetup -o 4194304 -f "$IMAGE_PATH"/boot.bin
-mount -t vfat /dev/loop0 /mnt/bootbin
+loop_name="$(losetup -f)"
+echo "Using $loop_name to mount boot.bin"
+losetup -o 4194304 "$loop_name" "$IMAGE_PATH"/boot.bin
+mount -t vfat "$loop_name" /mnt/bootbin
 
 rm -f /mnt/bootbin/*
 cp "$RES_PATH"/flash/fastboot.bin /mnt/bootbin/
@@ -20,4 +22,4 @@ mkimage -A arm64 -T script -C none -n "Boot script" -d "$RES_PATH"/flash/boot.cm
 
 ls -C /mnt/bootbin/
 umount /mnt/bootbin
-losetup -d /dev/loop0
+losetup -d "$loop_name"
