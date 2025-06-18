21b0ecaf1f51bdec28d8bd452fa70bcd89d58530
Dominic Frye <me@itsnebula.net>
2025-06-17 21:54:23 -0400

    chore: migrate to busybox

--- scripts/stages/00/30-xbps.sh
+++ scripts/stages/00/30-xbps.sh
@@ -4,11 +4,21 @@ curl -L https://repo-default.voidlinux.org/live/current/void-armv7l-ROOTFS-"$VOI
 
 xbps-install -r "$ROOTFS_PATH" -Suy xbps
 xbps-install -r "$ROOTFS_PATH" -uy
-xbps-install -r "$ROOTFS_PATH" -y base-files coreutils findutils diffutils dash bash grep gzip file sed mawk less \
-  util-linux which tar shadow e2fsprogs dosfstools procps-ng iproute2 iputils kmod eudev runit-void ifupdown curl nano
+xbps-install -r "$ROOTFS_PATH" -y base-files busybox-huge bash file util-linux shadow e2fsprogs dosfstools \
+  procps-ng iproute2 iputils kmod eudev runit-void ifupdown curl
 
 {
   echo "ignorepkg=wifi-firmware"
   echo "ignorepkg=iw"
   echo "ignorepkg=wpa_supplicant"
 } >> "$ROOTFS_PATH"/etc/xbps.d/ignore.conf
+
+"$HELPERS_PATH"/chroot_exec.sh /bin/sh -c "
+  for util in \$(/usr/bin/busybox --list); do
+    [ ! -f \"/usr/bin/\$util\" ] && /usr/bin/busybox ln -sfv busybox \"/usr/bin/\$util\"
+  done
+  install -dm1777 /tmp
+  xbps-reconfigure -fa
+"
+
+DEFAULT_SERVICES="${DEFAULT_SERVICES} busybox-ntpd"
--- scripts/stages/10/60-ntpd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-xbps-install -r "$ROOTFS_PATH" -y openntpd
-
-DEFAULT_SERVICES="${DEFAULT_SERVICES} openntpd"
