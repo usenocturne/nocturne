beee75f8e9ec244d022b3c231acb8e497dbe58cc
Dominic Frye <me@itsnebula.net>
2025-08-09 18:32:12 -0400

    chore: include version in update and image filenames

--- scripts/stages/40/30-copy.sh
+++ scripts/stages/40/30-copy.sh
@@ -2,7 +2,7 @@
 
 rm "$OUTPUT_PATH"/* 2> /dev/null || true
 
-mv "$IMAGE_PATH"/nocturne_update.tar.zst "$OUTPUT_PATH"/
+mv "$IMAGE_PATH"/nocturne_update_"$NOCTURNE_IMAGE_VERSION".tar.zst "$OUTPUT_PATH"/
 
 mv "$IMAGE_PATH"/system.ext4 "$IMAGE_PATH"/system_a.ext2
 cp "$IMAGE_PATH"/system_a.ext2 "$IMAGE_PATH"/system_b.ext2
@@ -12,4 +12,4 @@ cp boot_a.dump boot_b.dump bootloader.dump dtbo_a.dump dtbo_b.dump fip_a.dump fi
 cp "$RES_PATH"/flash/env.txt "$RES_PATH"/flash/logo.dump "$IMAGE_PATH"/
 
 cd "$IMAGE_PATH"/ || exit 1
-zip -r9 "$OUTPUT_PATH"/nocturne_image.zip .
+zip -r9 "$OUTPUT_PATH"/nocturne_image_"$NOCTURNE_IMAGE_VERSION".zip .
--- scripts/stages/40/40-output.sh
+++ scripts/stages/40/40-output.sh
@@ -5,8 +5,8 @@ color_echo ">> Uncompressed Sizes"
 color_echo "size of root partition: $SIZE_ROOT_FS ($(du -sh "$ROOTFS_PATH" | sed "s/\s.*//") used)\n" -Yellow
 
 color_echo ">> Compressed Sizes"
-color_echo "size of install zip: $(du -sh "$OUTPUT_PATH"/nocturne_image.zip | sed "s/\s.*//")" -Yellow
-color_echo "size of update: $(du -sh "$OUTPUT_PATH"/nocturne_update.tar.zst | sed "s/\s.*//")\n" -Yellow
+color_echo "size of install zip: $(du -sh "$OUTPUT_PATH"/nocturne_image_"$NOCTURNE_IMAGE_VERSION".zip | sed "s/\s.*//")" -Yellow
+color_echo "size of update: $(du -sh "$OUTPUT_PATH"/nocturne_update_"$NOCTURNE_IMAGE_VERSION".tar.zst | sed "s/\s.*//")\n" -Yellow
 
 color_echo "$WORK_PATH" -Yellow
 echo
