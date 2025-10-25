4a690f678ffc490e372734bee4fe5648a77f0443
Dominic Frye <me@itsnebula.net>
2025-10-24 20:35:19 -0400

    feat: add motd and adjust fastfetch

--- image/external/board/nocturne/post-build.sh
+++ image/external/board/nocturne/post-build.sh
@@ -3,5 +3,15 @@ set -ex
 
 TARGET_DIR="$1"
 
-sed -i "s|\${GIT_HASH}|$(git -C .. rev-parse HEAD)|g" "$TARGET_DIR"/etc/nocturne/version.json
-sed -i "s|\${BUILD_DATE}|$(date +%Y-%m-%d-%H-%M-%S)|g" "$TARGET_DIR"/etc/nocturne/version.json
+git_hash=$(git -C .. rev-parse HEAD)
+build_date=$(date +%Y-%m-%d-%H-%M-%S)
+
+sed -i "s|\${GIT_HASH}|${git_hash}|g" "$TARGET_DIR"/etc/nocturne/version.json
+sed -i "s|\${BUILD_DATE}|${build_date}|g" "$TARGET_DIR"/etc/nocturne/version.json
+
+version=$(cat "$TARGET_DIR"/etc/nocturne/version.json | jq -r '.shortVersion')
+
+sed -i "s|\${VERSION}|${version}|g" "$TARGET_DIR"/etc/motd
+sed -i "s|\${GIT_HASH}|${git_hash}|g" "$TARGET_DIR"/etc/motd
+
+sed -i "s|\${NOCTURNE_VERSION}|${version}|g" "$TARGET_DIR"/etc/fastfetch/config.jsonc
