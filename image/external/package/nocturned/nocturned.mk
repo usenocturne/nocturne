ac989690ca110bb76a252f1e93824dab69dc8886
Dominic Frye <me@itsnebula.net>
2025-12-25 14:11:07 -0500

    chore: bump to v4.0.0-beta5

--- image/external/package/nocturned/nocturned.mk
+++ image/external/package/nocturned/nocturned.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NOCTURNED_VERSION = e779384
+NOCTURNED_VERSION = e238f71
 NOCTURNED_SITE_METHOD = git
 NOCTURNED_SITE = ssh://git@github.com/usenocturne/nocturned-private.git
 
